b22fda66ef19196425f3ae9156c8feb2dc59bf33
Ruth Ashleigh <notruth@notruth.com>
2018-04-14 11:59:58 -0700

    Added data-value to cell, updated onClick to 1 (alive), then re-render

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     var myRows = [];
     for (let i=0; i<this.state.height; i++){
       for (let x=0; x<this.state.width; x++){
-        myRows.push(<Cell key={i+x} handleClick={this.handleClick} value={Math.round((Math.random()))}/>)
+        myRows.push(<Cell key={i+x} handleClick={this.handleClick} />)
       }
       myGridArray.push(myRows);
       myRows = [];
@@ -30,16 +30,19 @@ class App extends Component {
   }
 
   componentDidUpdate(){
-    console.log(this.state.board[0][1].props.value);
+    //console.log(this.state.board[0][1].props.value);
   }
 
 
   handleClick(e){
     e.preventDefault();
+    console.log("value before:" + e.target.getAttribute('data-value'));
+    e.target.setAttribute('data-value', 1);
+    let board = this.state.board.slice();
     this.setState({
-      alive: !this.state.alive,
+      board: board,
     });
-    console.log(this.state.alive);
+    console.log("value after: " + e.target.getAttribute('data-value'));
   }
 
   render() {
@@ -51,7 +54,7 @@ class App extends Component {
 
 function Cell(props) {
     return (
-      <div className="cell" onClick={props.handleClick}></div>
+      <div className="cell" onClick={props.handleClick} data-value={Math.round((Math.random()))}></div>
     )
 }
 
