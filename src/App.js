fef620c1b439772d146db03a4c01b1e9adcf93ce
Ruth Ashleigh <notruth@notruth.com>
2018-04-15 21:20:46 -0700

    Added function to find neighbour cells based on row/col

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ class App extends Component {
       height: 40,
       valBoard: [],
     };
-    this.handleClick = this.handleClick.bind(this)
-    this.clearAll = this.clearAll.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.clearAll = this.clearAll.bind(this);
+    this.neighbours = this.neighbours.bind(this);
   }
 
   componentDidMount(){
@@ -50,13 +51,49 @@ class App extends Component {
     this.setState({
       valBoard: gridVal
     })
+  }
 
+  neighbours(n,j){
+    let x = [];
+    let b = this.state.valBoard;
 
+    //bottom right corner
+    if ((n === 39) && (j === 39)){
+      x = [b[38][38],b[38][39],b[38][0],b[39][38],b[39][0],b[0][38],b[0][39],b[0][0]];
+    //bottom left corner
+    } else if ((n === 39) && (j === 0)){
+      x = [b[38][0], b[38][1], b[38][39], b[39][39], b[39][1], b[0][0], b[0][1], b[0][39]];
+    //top right corner
+    } else if ((n === 0) && (j === 39)){
+      x = [b[39][38], b[39][39], b[39][0], b[0][38], b[0][0], b[1][38], b[1][39], b[1][0]];
+    //top left corner
+    } else if ((n === 0) && (j === 0)){
+      x = [b[39][0], b[39][1], b[39][39], b[0][1], b[0][39], b[1][0], b[1][1], b[1][39]];
+    //rightmost
+    } else if (j === 39){
+      x = [b[n-1][39], b[n-1][38], b[n-1][0], b[n+1][39], b[n+1][38], b[n+1][0], b[n][38], b[n][0]];
+    //leftmost
+    } else if (j === 0){
+      x = [b[n-1][0], b[n-1][1], b[n-1][39], b[n+1][0], b[n+1][1], b[n+1][39], b[n][1], b[n][39]];
+    //bottom
+    } else if (n === 39){
+      x = [b[38][j-1], b[38][j], b[38][j+1], b[0][j-1], b[0][j], b[0][j+1], b[39][j-1], b[39][j+1]];
+    //top
+    } else if (n === 0){
+      x = [ b[39][j-1], b[39][j], b[39][j+1], b[1][j-1], b[1][j], b[1][j+1], b[0][j-1], b[0][j+1]];
+    // any non-edge
+    } else {
+      x = [b[n-1][j-1], b[n-1][j], b[n-1][j+1], b[n][j-1], b[n][j+1], b[n+1][j-1], b[n+1][j], b[n+1][j+1]];
+    }
+
+    let livingNeighbours = x.filter(Boolean);
+    return livingNeighbours.length;
   }
 
   handleClick(e){
-    let col = e.target.getAttribute('data-key');
-    let row = e.target.getAttribute('data-row');
+    let col = parseInt(e.target.getAttribute('data-key'),10);
+    let row = parseInt(e.target.getAttribute('data-row'),10);
+
     e.preventDefault();
     let board = this.state.valBoard.slice();
     board[row][col] = true;
